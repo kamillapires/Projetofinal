cbe1baf26e573aee5f8143e73184789ef8428f27
Kamilla Pires <kamilla.pires02@gmail.com>
2025-06-16 01:17:30 -0300

    Update listaexercicio4.py

--- listaexercicio4.py
+++ listaexercicio4.py
@@ -7,7 +7,7 @@ import streamlit as st
 """Este projeto tem como objetivo integrar análise de dados contábeis de empresas com indicadores econômicos, utilizando Python, Pandas, Ipeadata e Streamlit.
 """
 
-if st.checkbox("Mostrar enunciado"):
+if st.checkbox("Mostrar enunciado 1)"):
     st.write("1) Configure o título na barra do navegador, da página do projeto no Streamlit e descrição inicial do projeto (peso: 1,0)")
     st.write("- Título na barra (`page_title`): Lista de Exercícios 4")
     st.write("- Título da página (`header`): Projeto Final - Análise Contábil com Ajuste Econômico")
