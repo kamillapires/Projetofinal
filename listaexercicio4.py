e1697c6ef93dec4cc9f0ac01f7bace5ab37b2a54
Kamilla Pires <kamilla.pires02@gmail.com>
2025-06-15 23:46:32 -0300

    Update listaexercicio4.py

--- listaexercicio4.py
+++ listaexercicio4.py
@@ -110,8 +110,7 @@ st.dataframe(df2)
 
 """6) Crie gráfico de linha que apresente as variáveis Receita Líquida e Receita Real ao longo dos anos (no mesmo gráfico) (peso: 1,0)"""
 
-fig, ax = plt.subplots()
-plt.figure(figsize=(10, 6))
+plt.subplots(figsize=(10, 6))
 plt.grid(True)
 
 ax.plot(df2["Ano"], df2["Receita Líquida"], marker='o', label='Receita Líquida')
