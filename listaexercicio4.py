c14722a08689da6678542dbcf89a950586908d6b
Kamilla Pires <kamilla.pires02@gmail.com>
2025-06-16 00:18:51 -0300

    Update listaexercicio4.py

--- listaexercicio4.py
+++ listaexercicio4.py
@@ -44,7 +44,7 @@ df["Margem Líquida"] = df["Lucro Líquido"] / df["Receita Líquida"] * 100
 df["ROA"] = df["Lucro Líquido"] / df["Ativo Total"] * 100
 
 if st.checkbox('Mostrar dataframe'):
-    st.write("Nova df com as novas colunas Margem Líquida e ROA adicionadas:")
+    st.write("DataFrame atualizado com as colunas Margem Líquida e ROA adicionadas:")
     st.dataframe(df)
 
 df_agrupado = df.groupby('Ano')[['Margem Líquida', 'ROA']].mean().reset_index()
