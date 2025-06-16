996b5e815b6adfba1e1531556b6735e304c8a1a8
Kamilla Pires <kamilla.pires02@gmail.com>
2025-06-16 00:22:44 -0300

    Update listaexercicio4.py

--- listaexercicio4.py
+++ listaexercicio4.py
@@ -104,8 +104,12 @@ st.dataframe(ipca_dados)
 """
 
 #Combinado as duas df
+
 df2 = pd.merge(df, ipca_dados, on='Ano')
-st.dataframe(df2)
+if st.checkbox('Mostrar dataframe'):
+    st.write("DFS UNIFICADAS:")
+    st.dataframe(df2)
+
 
 #Nova df com a nova coluna de Receita Real 
 df2["Receita Real"] = df2["Receita Líquida"] - (df2["Receita Líquida"] * (df2["IPCA"]/100))
