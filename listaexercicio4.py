281c404ca32df80c395e5530955023cded89135c
Kamilla Pires <kamilla.pires02@gmail.com>
2025-06-16 00:56:31 -0300

    Update listaexercicio4.py

--- listaexercicio4.py
+++ listaexercicio4.py
@@ -52,7 +52,7 @@ if mostrar_df:
 else:
     st.write("Clique acima para exibir o dataframe atualizado.")
 
-st.subheader("Gráfico de Margem Líquida e ROA ao longo dos anos:")
+st.subheader("Gráfico de Indicadores: Margem Líquida e ROA ao Longo do Tempo:")
 df_agrupado = df.groupby('Ano')[['Margem Líquida', 'ROA']].mean().reset_index()
 
 anos = df_agrupado['Ano'].unique()  # Garante que todos os anos estejam listados
