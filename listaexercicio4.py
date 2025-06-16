9867eb442a231b28359205b8b4b13e3493733f2e
Kamilla Pires <kamilla.pires02@gmail.com>
2025-06-16 03:00:39 -0300

    Update listaexercicio4.py

--- listaexercicio4.py
+++ listaexercicio4.py
@@ -8,7 +8,7 @@ st.header("📊 Projeto Final – Análise Contábil com Ajuste Econômico")
 st.write("Este projeto tem como objetivo integrar análise de dados contábeis de empresas com indicadores econômicos, utilizando Python, Pandas, Ipeadata e Streamlit.")
 
 if st.checkbox("Mostrar enunciado 1)"):
-    st.write("📝 1) Configure o título na barra do navegador, da página do projeto no Streamlit e descrição inicial do projeto (peso: 1,0).")
+    st.write("📝 1) Configure o título na barra do navegador, da página do projeto no Streamlit e descrição inicial do projeto. (peso: 1,0)")
     st.write("- Título na barra (`page_title`): Lista de Exercícios 4.")
     st.write("- Título da página (`header`): Projeto Final - Análise Contábil com Ajuste Econômico.")
     st.write("- Descrição projeto (`write`): Este projeto tem como objetivo integrar análise de dados contábeis de empresas com indicadores econômicos, utilizando Python, Pandas, Ipeadata e Streamlit.")
@@ -16,7 +16,7 @@ else:
     st.write("📌 Enunciado ocultado.")
 
 if st.checkbox("Mostrar enunciado 2)"):
-    st.write("📝 2) Importe os dados do arquivo empresas_dados.csv utilizando pandas e apresente todas as linhas da df (peso: 1,0).")
+    st.write("📝 2) Importe os dados do arquivo empresas_dados.csv utilizando pandas e apresente todas as linhas da df. (peso: 1,0)")
     st.write("Dica: Utilize `head(len(df))")
 else:
     st.write("📌 Enunciado ocultado.")
@@ -29,7 +29,7 @@ df = pd.read_csv(arquivo, sep=";")
 st.dataframe(df.head(len(df)))
 
 if st.checkbox("Mostrar enunciado 3)"):
-    st.write("📝 3) Calcule os indicadores Margem Líquida e ROA e salve como novas coluna da df. Depois apresente os dois indicadores no mesmo gráfico de linhas, agrupado por Ano  (peso: 1,0).")
+    st.write("📝 3) Calcule os indicadores Margem Líquida e ROA e salve como novas coluna da df. Depois apresente os dois indicadores no mesmo gráfico de linhas, agrupado por Ano. (peso: 1,0)")
     st.write("- Margem Líquida = Lucro Líquido / Receita Líquida * 100")
     st.write("- ROA = Lucro Líquido / Ativo Total *  100")
 else:
@@ -72,9 +72,9 @@ st.pyplot(fig)
 
 if st.checkbox("Mostrar enunciado 4)"):
     st.write("📝 4) Utilize o pacote ipeadatapy e faça busca para encontrar o indicador que traga o IPCA, taxa de variação, em % e anual: (peso: 2,0).")
-    st.write("- Baixe os dados no período de 2010 a 2024")
-    st.write("- Altere o nome da coluna 'YEAR' para 'Ano'")
-    st.write("- Altere o nome da coluna 'VALUE ((% a.a.))' para 'IPCA'")
+    st.write("- Baixe os dados no período de 2010 a 2024.")
+    st.write("- Altere o nome da coluna 'YEAR' para 'Ano'.")
+    st.write("- Altere o nome da coluna 'VALUE ((% a.a.))' para 'IPCA'.")
     st.write("- Apresente a df para checar se tudo deu certo.")
 else:
     st.write(" 📌 Enunciado ocultado.")
@@ -99,10 +99,10 @@ ipca_dados = ipca_dados.rename(columns={"YEAR": "Ano", "VALUE ((% a.a.))": "IPCA
 st.dataframe(ipca_dados)
 
 if st.checkbox("Mostrar enunciado 5)"):
-    st.write("📝 5) Combine as duas df (Excel e IPEA) em uma nova df e calcule nova coluna chamada Receita Real (peso: 2,0)")
-    st.write("- Utilize a função `pd.merge()` para unificar as duas df utiilizando a coluna Ano como conexão (chave primária) entre elas")
-    st.write("- Crie nova coluna chamada Receita Real que será o resultado da Receita Líquida de cada ano deduzido o IPCA do ano: `Receita Real = Receitta Líquida - ( Receita Líquida * (IPCA/100) )")
-    st.write("- Apresente a nova df combinada")
+    st.write("📝 5) Combine as duas df (Excel e IPEA) em uma nova df e calcule nova coluna chamada Receita Real. (peso: 2,0)")
+    st.write("- Utilize a função `pd.merge()` para unificar as duas df utiilizando a coluna Ano como conexão (chave primária) entre elas.")
+    st.write("- Crie nova coluna chamada Receita Real que será o resultado da Receita Líquida de cada ano deduzido o IPCA do ano: `Receita Real = Receita Líquida - ( Receita Líquida * (IPCA/100) ).")
+    st.write("- Apresente a nova df combinada.")
     
 else:
     st.write(" 📌 Enunciado ocultado.")
@@ -123,7 +123,7 @@ st.subheader(" 💾 Atualização dos Dados: Inclusão da Coluna 'Receita Real'"
 st.dataframe(df2)
 
 if st.checkbox("Mostrar enunciado 6)"):
-    st.write("📝 6) Crie gráfico de linha que apresente as variáveis Receita Líquida e Receita Real ao longo dos anos (no mesmo gráfico) (peso: 1,0)")
+    st.write("📝 6) Crie gráfico de linha que apresente as variáveis Receita Líquida e Receita Real ao longo dos anos (no mesmo gráfico). (peso: 1,0)")
 else:
     st.write(" 📌 Enunciado ocultado.")
 
